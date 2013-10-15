12a0312445bda4d2c9f0ce988677b4955a7c5fce
Carlos Vilhena <carlosvilhena@gmail.com>
2013-10-15 10:57:44 +0100

    use postgres in dev mode as well

--- config.js
+++ config.js
@@ -1,48 +1,26 @@
-// # Ghost Configuration
-// Setup your Ghost install for various environments
-
 var path = require('path'),
     config;
 
 config = {
-    // ### Development **(default)**
     development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
         url: 'http://localhost:2368',
-
-        // Example mail config
-        // Visit http://docs.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
-
         database: {
-            client: 'sqlite3',
+            client: 'postgres',
             connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
+                host: 'localhost',
+                user: 'carvil_blog',
+                password: '',
+                database: 'carvil_blog_development',
+                port: 5432
             },
             debug: false
         },
         server: {
-            // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
             port: '2368'
         }
     },
 
-    // ### Production
-    // When running Ghost in the wild, use the production environment
-    // Configure your URL and mail settings here
     production: {
         url: 'http://www.carlosvilhena.com',
         mail: {},
@@ -62,11 +40,6 @@ config = {
         }
     },
 
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
     testing: {
         url: 'http://127.0.0.1:2369',
         database: {
@@ -81,8 +54,6 @@ config = {
         }
     },
 
-    // ### Travis
-    // Automated testing run through Github
     travis: {
         url: 'http://127.0.0.1:2368',
         database: {
@@ -98,5 +69,4 @@ config = {
     }
 };
 
-// Export config
 module.exports = config;
